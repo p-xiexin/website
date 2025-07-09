97d0027a4755dd4ed9dd20235001d14528c91314
p-xiexin <p-xiexin@users.noreply.github.com>
2025-07-09 16:30:15 +0000

    deploy: 97300ccc483db1a6b80191d0dff2affb606690f0

---
+++ _app/immutable/chunks/Pxlebmo3.js
@@ -0,0 +1,1 @@
+import{b as e}from"./CWZ9FyE3.js";const r="derek-portfolio",s=[{name:"Home",href:`${e}/`},{name:"About",href:`${e}/about`},{name:"Projects",href:`${e}/projects`},{name:"Blogs",href:`${e}/blogs`}];export{s as n,r as u};
