74750aaab667da1ab4ae1f26b268b81d5239b768
p-xiexin <1014575731@qq.com>
2025-07-10 00:18:34 +0800

    delete mdvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-  extensions: ['.md']
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', '.md'],
-  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  extensions: ['.svelte'],
+  preprocess: [vitePreprocess()],
   kit: {
     adapter: adapter({
       pages: 'build',
