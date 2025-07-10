0b2a927d38d27cc350d58dcd864098a6a422a022
p-xiexin <p-xiexin@users.noreply.github.com>
2025-07-10 17:11:23 +0000

    deploy: b80d13d8f7eac7bf4860d7cbe64163b6e4e35e4c

---
+++ _app/immutable/chunks/CT6MwZJN.js
@@ -0,0 +1,1 @@
+import{b as e}from"./C_3UxJV5.js";const r="derek-portfolio",s=[{name:"Home",href:`${e}/`},{name:"About",href:`${e}/about`},{name:"Projects",href:`${e}/projects`},{name:"Blogs",href:`${e}/blogs`}];export{s as n,r as u};
