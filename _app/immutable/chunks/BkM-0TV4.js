8095e12064e817bc89ade5c52b95775c289d37e7
p-xiexin <p-xiexin@users.noreply.github.com>
2025-08-16 13:29:23 +0000

    deploy: 9fc1605df7d8408056ed5f0ae5f39621f6cbbd64

--- _app/immutable/chunks/BkM-0TV4.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./CTZKF4rf.js";const r="derek-portfolio",s=[{name:"Home",href:`${e}/`},{name:"About",href:`${e}/about`},{name:"Projects",href:`${e}/projects`},{name:"Blogs",href:`${e}/blogs`}];export{s as n,r as u};
